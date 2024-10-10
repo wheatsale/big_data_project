c4ebbe8432944a79ffa001bbc713a3f3ed0331e3
Ellie Rorer <ellie.rorer@gmail.com>
2024-10-10 14:46:12 -0400

    Add enough functionality for web app assignment.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::{get, post},
+    extract::Form,
+    response::Html,
+    Router,
+};
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+#[allow(dead_code)]
+struct Input {
+    user_input: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async {
+        Html(r#"
+        <form action="/echo_user_input" method="POST">
+            <input name="user_input">
+            <input type="submit" value="Submit!">
+        </form>
+        "#)
+    }))
+
+    .route("/echo_user_input", post(accept_form));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn accept_form(Form(input): Form<Input>) -> String {
+    input.user_input
 }
