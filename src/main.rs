8c3cc92847efbe7a43b1e797f84026357807d2aa
Ellie Rorer <ellie.rorer@gmail.com>
2024-10-14 11:15:12 -0400

    Create function for serving static files included in the project directory.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 use axum::{
-    routing::{get, post},
-    extract::Form,
-    response::Html,
-    Router,
+    extract::Form, http::StatusCode, response::{Html, IntoResponse}, routing::{get, post}, Router
 };
 use serde::Deserialize;
-use std::env;
-
+use std::{
+    env, fs::File, io::Read, path::PathBuf
+};
 
 #[derive(Deserialize, Debug)]
 #[allow(dead_code)]
@@ -14,18 +12,37 @@ struct Input {
     user_input: String,
 }
 
+// Returns an HTML response if the static file is found and a 404 response otherwise
+fn static_file(path: &str) -> impl IntoResponse {
+    // Use CARGO_MANIFEST_DIR if run via "cargo run" and look two directories down if run from the
+    // release folder (ie. via Heroku).
+    let dir = match option_env!("CARGO_MANIFEST_DIR") {
+        Some(env) => env,
+        None => "../.."
+    };
+
+    let path = PathBuf::from(format!("{dir}/{path}"));
+
+    match File::open(path) {
+        Ok(mut file) => {
+            let mut buffer = String::new();
+
+            match file.read_to_string(&mut buffer) {
+                Ok(_) => Html(buffer).into_response(),
+                Err(_) => (StatusCode::NOT_FOUND, "file not found").into_response()
+            }
+        },
+        Err(_) => (StatusCode::NOT_FOUND, "file not found").into_response()
+    }
+}
+
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async {
-        Html(r#"
-        <form action="/echo_user_input" method="POST">
-            <input name="user_input">
-            <input type="submit" value="Submit!">
-        </form>
-        "#)
-    }))
+    let app = Router::new().route("/", get(root));
 
-    .route("/echo_user_input", post(accept_form));
+    let app = app.route("/echo_user_input", post(accept_form));
+
+    let app = app.fallback(handler_404);
 
     let port = match env::var("PORT") {
         Ok(val) => val,
@@ -36,6 +53,14 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
+async fn root() -> impl IntoResponse {
+    static_file("resources/static/templates/index.html")
+}
+
 async fn accept_form(Form(input): Form<Input>) -> String {
     input.user_input
+}
+
+async fn handler_404() -> impl IntoResponse {
+    (StatusCode::NOT_FOUND, "file not found")
 }
