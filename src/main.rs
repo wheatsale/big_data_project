a1acc18b4516e54178bbff58faccf674ab429cdd
Ellie Rorer <ellie.rorer@gmail.com>
2024-10-14 17:58:58 -0400

    Added template for results that inherits from index.

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,11 @@ use serde::Deserialize;
 use askama_axum::Template;
 use std::env;
 
-#[derive(Deserialize, Debug)]
-#[allow(dead_code)]
-struct Input {
-    user_input: String,
-}
-
-#[derive(Template)]
-#[template(path = "index.html")]
-struct IndexTemplate<'a> {
-    test: &'a str,
-}
-
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/", get(root));
 
-    let app = app.route("/echo_user_input", post(accept_form));
+    let app = app.route("/results", post(search));
 
     let app = app.fallback(handler_404);
 
@@ -34,12 +22,40 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate { }
+
 async fn root() -> impl IntoResponse {
-    IndexTemplate { test: "bleh" }
+    IndexTemplate { }
 }
 
-async fn accept_form(Form(input): Form<Input>) -> String {
-    input.user_input
+#[derive(Deserialize, Debug)]
+#[allow(dead_code)]
+struct SearchInput {
+    provider_name: String,
+    care_type: String,
+    subreddits: String,
+}
+
+#[derive(Template)]
+#[template(path = "results.html")]
+struct ResultsTemplate {
+    provider: String,
+    care: String,
+    subreddits: String,
+}
+
+async fn search(Form(search): Form<SearchInput>) -> impl IntoResponse {
+    let provider_result = format!("You searched for {}", search.provider_name);
+    let care_result = format!("You searched for {}", search.care_type);
+    let subreddit_results = format!("You searched for {}", search.subreddits);
+
+    ResultsTemplate{ 
+        provider: provider_result,
+        care: care_result,
+        subreddits: subreddit_results
+    }
 }
 
 async fn handler_404() -> impl IntoResponse {
